ae762e0e7ce5f8d04280f4b73e8f4f8a2161190f
aadarshreddydepa <aadarshreddydepa@gmail.com>
2025-09-28 16:47:48 +0530

    added schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,64 @@
+# app/schemas.py
+from pydantic import BaseModel,EmailStr
+from typing import Optional,List
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    full_name: Optional[str] = None
+    phone_number: Optional[str] = None
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+class UserResponse(BaseModel):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    role: str
+
+    class Config:
+        orm_mode = True
+    
+class UserUpdate(BaseModel):
+    eamil: Optional[EmailStr] = None
+    full_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    role: Optional[str] = None  
+
+class FarmBase(BaseModel):
+    farm_name: str
+    location: Optional[str] = None
+    soil_type: Optional[str] = None
+    area: Optional[float] = None
+
+class FarmCreate(FarmBase):
+    pass
+
+class FarmBase(BaseModel):
+    farm_name: str
+    location: Optional[str] = None
+    soil_type: Optional[str] = None
+    area: Optional[float] = None
+
+class FarmCreate(FarmBase):
+    pass
+
+class FarmResponse(FarmBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
